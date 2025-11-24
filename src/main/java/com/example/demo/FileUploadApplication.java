122521e49a5622eae260406e1129092d87b42221
chargeahead <35832487+chargeahead@users.noreply.github.com>
2025-11-24 01:41:21 +0000

    Upgraded spring boot

--- src/main/java/com/example/demo/FileUploadApplication.java
+++ src/main/java/com/example/demo/FileUploadApplication.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
